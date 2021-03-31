d7302b226d1040e7e6da701ec4db191363b0f924
Your Name <you@example.com>
2021-03-30 21:46:39 -0300

    Multiple button sounds. CLose #4.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -9,9 +9,41 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(child: Text(adjectives.last)),
+          child: Column(
+            children: [
+              Butao("note1.wav", Colors.red),
+              Butao("note2.wav", Colors.orange),
+              Butao("note3.wav", Colors.yellow),
+              Butao("note4.wav", Colors.green),
+              Butao("note5.wav", Colors.greenAccent),
+              Butao("note6.wav", Colors.blue),
+              Butao("note7.wav", Colors.purple),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class Butao extends StatelessWidget {
+  String fileName;
+  Color color;
+
+  // constructor
+  Butao(String fileName, Color color) {
+    this.fileName = fileName;
+    this.color = color;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      color: color,
+      onPressed: () {
+        final AudioCache player = AudioCache();
+        player.play(fileName);
+      },
     );
   }
 }
